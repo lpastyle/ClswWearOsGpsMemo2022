ceaceafe23ce242165ec4137ceb76f489effd1b2
lpastyle <lpastorelli@lpastyle.com>
2022-10-06 10:44:29 +0200

    Added wear recycler view

---
+++ app/src/main/java/com/example/clswwearosgpsmemo2022/GpsLocationAdapter.java
@@ -0,0 +1,59 @@
+package com.example.clswwearosgpsmemo2022;
+
+import android.content.Context;
+import android.location.Location;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.wear.widget.WearableRecyclerView;
+
+import com.example.clswwearosgpsmemo2022.databinding.ItemGpsLocationBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GpsLocationAdapter extends WearableRecyclerView.Adapter<GpsLocationAdapter.GpsLocationViewHolder> {
+    private static final String DMYHMS_DATE_FORMAT = "dd/MM/yyyy-HH':'mm':'ss";
+
+    // Data model
+    ArrayList<Location> locations;
+    Context context;
+
+    public GpsLocationAdapter(ArrayList<Location> locations, Context context) {
+        this.locations = locations;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public GpsLocationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemGpsLocationBinding binding = ItemGpsLocationBinding.inflate(layoutInflater, parent, false);
+        return new GpsLocationViewHolder(binding);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GpsLocationViewHolder holder, int position) {
+        Location curItem = locations.get(position);
+        Date date = new Date(curItem.getTime());
+        holder.binding.timeStampTv.setText(context.getString(R.string.timestamp_format,position+1, DateFormat.format(DMYHMS_DATE_FORMAT, date).toString()));
+        holder.binding.latitudeTv.setText(context.getString(R.string.latitude_format, curItem.getLatitude()));
+        holder.binding.longitudeTv.setText(context.getString(R.string.longitude_format, curItem.getLongitude()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return locations.size();
+    }
+
+    public static class GpsLocationViewHolder extends WearableRecyclerView.ViewHolder {
+        ItemGpsLocationBinding binding;
+        public GpsLocationViewHolder(@NonNull ItemGpsLocationBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/example/clswwearosgpsmemo2022/MainActivity.java
+++ app/src/main/java/com/example/clswwearosgpsmemo2022/MainActivity.java
@@ -1,23 +1,70 @@
 package com.example.clswwearosgpsmemo2022;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.wear.widget.WearableLinearLayoutManager;
+import androidx.wear.widget.WearableRecyclerView;
 
 import com.example.clswwearosgpsmemo2022.databinding.ActivityMainBinding;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Date;
 
-    private TextView mTextView;
+public class MainActivity extends Activity {
+    private static final String LOG_TAG=MainActivity.class.getSimpleName();
+    private static final String LOC_PROVIDER = "WATCH";
+
+    // Recycler view data model
+    ArrayList<Location> locations = new ArrayList<>();
+    GpsLocationAdapter adapter;
+
+    // keep view binding
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG,"onCreate()");
 
+        // Init view binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        // dummy init for test purpose only
+        locations.add(makeLoc(42.12812921d, 6.22987212d));
+        locations.add(makeLoc(43.24648328d, 7.53783836d));
+        locations.add(makeLoc(44.39800122d, 8.40192765d));
+        locations.add(makeLoc(45.83232627d, 9.12763238d));
+
+        // Wearable recycler view init
+        binding.gpsLocationWrv.setEdgeItemsCenteringEnabled(true);
+        binding.gpsLocationWrv.setLayoutManager(new WearableLinearLayoutManager(this));
+        adapter = new GpsLocationAdapter(locations,this);
+        binding.gpsLocationWrv.setAdapter(adapter);
+
+    }
+
+    /* *********************************************************************************************
+     * HELPER METHODS
+     * *********************************************************************************************
+     */
+    private boolean hasGps() {
+        Log.d(LOG_TAG, "hasGps()");
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
+
+    private Location makeLoc(double lat, double lon) {
+        Location location = new Location(LOC_PROVIDER);
+        Date now = new Date();
+        location.setLatitude(lat);
+        location.setLongitude(lon);
+        location.setTime(now.getTime());
+        return location;
     }
 }
